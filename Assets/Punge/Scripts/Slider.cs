6f981daef9934a731d6ba4172a78ac54ed52af66
astrellon <sovereign250@gmail.com>
2013-10-29 22:17:29 +1100

    - Added a basic UI 3d slider.

---
+++ Assets/Punge/Scripts/Slider.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slider : MonoBehaviour {
+	
+	protected GameObject thumb;
+	protected TextMesh valueText;
+	
+	protected float sliderValue = 0.5f;
+	public float SliderValue 
+	{
+		get
+		{
+			return sliderValue;
+		}
+		set
+		{
+			if (value < MinValue) {
+				sliderValue = MinValue;
+			}
+			else if (value > MaxValue) {
+				sliderValue = MaxValue;	
+			}
+			else {
+				sliderValue = value;	
+			}
+		}
+	}
+	public float MinValue = 0.0f;
+	public float MaxValue = 1.0f;
+	protected float size = 1.0f;
+	protected Transform backQuad;
+	public string Label = "Slider";
+	
+	// Use this for initialization
+	void Start () {
+		thumb = transform.FindChild("Thumb").gameObject;
+		valueText = transform.FindChild("Value").GetComponent<TextMesh>();
+		backQuad = transform.FindChild("Back").transform;
+		backQuad.gameObject.AddComponent(typeof(SliderBack));
+		backQuad.gameObject.GetComponent<SliderBack>().PercentCallback = PercentCallback;
+		size = backQuad.localScale.x - 0.5f;
+		
+		transform.FindChild("Label").GetComponent<TextMesh>().text = Label;
+		UpdateThumb();
+	}
+	
+	void PercentCallback(float percent) {
+		SliderValue = percent * (MaxValue - MinValue) + MinValue;
+		UpdateThumb();
+	}
+	
+	void UpdateThumb() {
+		float percent = (SliderValue - MinValue) / (MaxValue - MinValue);
+		thumb.transform.localPosition = new Vector3((percent - 0.5f) * size, 0.0f, 0.0f);
+		valueText.text = SliderValue.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Punge/Scripts/SliderBack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderBack : MonoBehaviour {
+	
+	private Plane hitPlane;
+	private bool mouseDown = false;
+	void OnMouseDown() {
+		mouseDown = true;
+		hitPlane = new Plane(transform.forward, transform.position);
+		DoCast ();
+	}
+	
+	public delegate void SliderPercent(float percent);
+	public SliderPercent PercentCallback;
+	
+	private void DoCast() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		float dist = -1.0f;
+		if (hitPlane.Raycast(ray, out dist)) {
+			Vector3 localPoint = transform.worldToLocalMatrix.MultiplyPoint3x4(ray.GetPoint(dist));
+			float percent = localPoint.x + 0.5f;
+			if (PercentCallback != null) {
+				PercentCallback(percent);	
+			}
+		}
+	}
+	
+	void Update() {
+		if (mouseDown) {
+			DoCast();
+			if (!Input.GetMouseButton(0)) {
+				mouseDown = false;	
+			}
+		}
+	}
+}
