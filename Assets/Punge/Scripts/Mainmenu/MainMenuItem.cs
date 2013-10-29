64e046c4e67031f94e4e8c10bdc761f47e95db76
astrellon <sovereign250@gmail.com>
2013-10-29 13:27:27 +1100

    - Beginnings of a main menu with options.

---
+++ Assets/Punge/Scripts/Mainmenu/MainMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnMouseOver () {
+		this.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+	}
+	void OnMouseExit() {
+		this.transform.localScale = new Vector3(1f, 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
